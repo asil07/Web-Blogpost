5451179096ca1557705b1d5a78b4a4ec2f4cf3d1
Asilbek <asilbekyuldashev07@gmail.com>
2021-04-11 11:03:57 +0900

    data required

--- forms.py
+++ forms.py
@@ -22,7 +22,7 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[Email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Let's go!")
 
